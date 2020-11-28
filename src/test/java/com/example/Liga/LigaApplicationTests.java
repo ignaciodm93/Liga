2af589bf2801abff3e7888b85b13f204c88ff8f9
Ignacio Della Morte <ignaciodm93@gmail.com>
2020-11-28 19:04:58 -0300

    First update

---
+++ src/test/java/com/example/Liga/LigaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Liga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LigaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
